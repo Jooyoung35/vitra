d54b1576ecab35bb823d934e3fc16ec1cd1a2aaa
Jooyoung35 <jykimjulia@gmail.com>
2025-08-16 16:17:38 +0900

    인터랙션 추가

---
+++ index.js
@@ -0,0 +1,48 @@
+
+//product content 나타나기
+    const ani2 = gsap.timeline();
+    ani2.from(".sec-title", {y: 150, autoAlpha:0}) // opacity 0 시작 > 점점 나타나게
+        .from(".wrap-pro-cont", {y: 200, autoAlpha:0}) 
+
+    ScrollTrigger.create({
+        animation: ani2,
+        trigger: ".product", //해당영역이 화면에 들어오면 애니메이션 작동
+        start: "-=600 top", //시작점 기준보다 700px위에
+        end: "+=400",
+        scrub: 1,
+        pin: false,
+        anticipatePin: 1,
+        markers: false,
+    });
+
+
+    //enviromental 나타나기
+    const ani3 = gsap.timeline();
+    ani3.from(".environ-img", {x: -150, autoAlpha:0}) // opacity 0 시작 > 점점 나타나게
+        .from(".environ-txt", {x: 200, autoAlpha:0}) 
+
+    ScrollTrigger.create({
+        animation: ani3,
+        trigger: ".environmental", //해당영역이 화면에 들어오면 애니메이션 작동
+        start: "-=600 top", //시작점 기준보다 700px위에
+        end: "+=400",
+        scrub: 1,
+        pin: false,
+        anticipatePin: 1,
+        markers: false,
+    });
+
+    //campus 나타나기
+    const ani4 = gsap.timeline();
+    ani4.from(".wrap-cam", {y: 150, autoAlpha:0}) // opacity 0 시작 > 점점 나타나게
+
+    ScrollTrigger.create({
+        animation: ani4,
+        trigger: ".campus", //해당영역이 화면에 들어오면 애니메이션 작동
+        start: "-=600 top", //시작점 기준보다 700px위에
+        end: "+=400",
+        scrub: 1,
+        pin: false,
+        anticipatePin: 1,
+        markers: false,
+    });
