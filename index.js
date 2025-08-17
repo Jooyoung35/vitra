514640262cea34b192648a0646918ca4cc40bfb4
Jooyoung35 <jykimjulia@gmail.com>
2025-08-17 16:48:02 +0900

    반응형 작업완료

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+ //마우스 스크롤했을때 헤더에 배경색상넣기
+    window.addEventListener('scroll', function () { 
+    const header = document.getElementById('header');
+    if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+    } else {
+    header.classList.remove('scrolled');
+    }
+    });
+
+
+
+
+
+
+
+//모바일 gnb 나타나기 (메뉴버튼 클릭시 보이게)
+    const hamBtn = document.querySelector('.ham-btn');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamBtn.addEventListener('click', () => {
+      mobileMenu.classList.toggle('active');
+    });
+
+
 
 //product content 나타나기
     const ani2 = gsap.timeline();
@@ -18,8 +43,8 @@
 
     //enviromental 나타나기
     const ani3 = gsap.timeline();
-    ani3.from(".environ-img", {x: -150, autoAlpha:0}) // opacity 0 시작 > 점점 나타나게
-        .from(".environ-txt", {x: 200, autoAlpha:0}) 
+    ani3.from(".environ-img", {xPercent: -30, autoAlpha:0}) // opacity 0 시작 > 점점 나타나게
+        .from(".environ-txt", {xPercent: 30, autoAlpha:0}) 
 
     ScrollTrigger.create({
         animation: ani3,
@@ -32,6 +57,8 @@
         markers: false,
     });
 
+
+
     //campus 나타나기
     const ani4 = gsap.timeline();
     ani4.from(".wrap-cam", {y: 150, autoAlpha:0}) // opacity 0 시작 > 점점 나타나게
@@ -39,7 +66,7 @@
     ScrollTrigger.create({
         animation: ani4,
         trigger: ".campus", //해당영역이 화면에 들어오면 애니메이션 작동
-        start: "-=600 top", //시작점 기준보다 700px위에
+        start: "-=500 top", //
         end: "+=400",
         scrub: 1,
         pin: false,
